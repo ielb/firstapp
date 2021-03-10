a5c6dd76e193d7f36b6d725a4e0d5568e4ac1d22
ielb <i.ebouhatii@gmail.com>
2021-03-10 14:37:58 +0100

    main textg

--- src/components/HeroSection/index.js
+++ src/components/HeroSection/index.js
@@ -1,19 +1,51 @@
-import React from 'react'
-import Video from '../../video/video.mp4'
+import React , {useState} from 'react';
+import Video from '../../video/video.mp4';
+import { Button } from '../ButtonElements';
 import {
     HeroContainer,
     HeroBg,
-    VideoBg
+    VideoBg,
+    HeroBtnWrapper,
+    HeroContent,
+    HeroH1,
+    HeroP,
+    ArrowForward,
+    ArrowRight
 
 }from './HeroElements'
 
+
 const HeroSection=()=> {
+    const [hover ,setHover] = useState(false);
+
+    const onHover= ()=>{
+        setHover(!hover);
+    }
+
     return (
         <HeroContainer>
             <HeroBg>
                 <VideoBg autoPlay loop muted src={Video} type="video/mp4"/>
             </HeroBg>
+            <HeroContent>
+                <HeroH1>Virtual Banking  Made Easey</HeroH1>
+                <HeroP>
+                    Sign up for new account today and receive $250 in credit towards your next payment.
+                </HeroP>
+                
+                <HeroBtnWrapper>
+                    <Button to="signup"
+                    onMouseEnter={onHover}
+                    onMouseLeave={onHover}
+                    primary="true"
+                    dark = "true"
+                    >
+                        Get started{hover ? <ArrowForward/> :<ArrowRight/>}
+                    </Button>
+                </HeroBtnWrapper>
+            </HeroContent>
         </HeroContainer>
+        
     );
 }
 
